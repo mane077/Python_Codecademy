55302e5c4d92dfa4be2f1d143cbc4bcab0bb1079
mane_07 <mane_ghost@yahoo.com>
2025-04-07 11:03:10 +0300

    Delete custom_module.py

--- custom_module.py
+++
@@ -1,2 +0,0 @@
-def generate_time_travel_message(year, destination, cost):
-  return print(f"Pack your bags! You're traveling to {destination} in the year {year}. The cost of this trip will be ${cost}.")
